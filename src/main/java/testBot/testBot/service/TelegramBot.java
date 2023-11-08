5071a43f6ad762cbd4106f3e8fec0f9b16110e4f
theforps <theforps@gmail.com>
2023-11-08 23:59:36 +0300

    :)

--- src/main/java/testBot/testBot/service/TelegramBot.java
+++ src/main/java/testBot/testBot/service/TelegramBot.java
@@ -1,31 +1,27 @@
 package testBot.testBot.service;
 
-
-import org.springframework.context.MessageSource;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.*;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import testBot.testBot.config.BotConfig;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     private final BotConfig config;
+    private final JSONObject json = new JSONObject(new String(Files.readAllBytes(Paths.get("src/main/resources/data.json")), StandardCharsets.UTF_8));
 
-    public TelegramBot(BotConfig config) {
+    public TelegramBot(BotConfig config) throws IOException {
         this.config = config;
     }
 
@@ -33,79 +29,36 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         Message message = update.getMessage();
-        Long chatId = update.getMessage().getChatId();
-        Integer messageId = update.getMessage().getMessageId();
+        Long chatId = message.getChatId();
+        Integer messageId = message.getMessageId();
 
-        if(message != null && message.hasText())
+        if(message.hasText())
         {
-            if (message.getText().equals("/start")) {
-                startCommand(update);
-            } else {
-                editMessage(update);
+            if (message.getText().equals("/start"))
+            {
+                firstMessage(update);
+            }
+            else
+            {
                 deleteMessage(chatId, messageId);
             }
         }
     }
 
-    private void startCommand(Update update)
-    {
-        sendMessage(update);
-    }
-
-    private void sendMessage(Update update) {
+    private void firstMessage(Update update)  {
 
         Long chatId = update.getMessage().getChatId();
-
         SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(String.valueOf(chatId));
-        sendMessage.setText("Привет!");
 
-        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        InlineKeyboardButton but = new InlineKeyboardButton();
-        but.setText(LocalDateTime.now().toString());
-        but.setCallbackData("test");
-        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        String text = json.getJSONObject("firstMessage").getString("text");
+
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(text);
 
         try {
             execute(sendMessage);
         }
         catch (TelegramApiException ignored) {}
-    }
-
-    private void editMessage(Update update)
-    {
-        Long chatId = update.getMessage().getChatId();
-        Integer messageId = update.getMessage().getMessageId()-1;
-
-        EditMessageText editMessageText = new EditMessageText();
-        EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup();
-
-        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        InlineKeyboardButton but = new InlineKeyboardButton();
-        but.setText(LocalDateTime.now().toString());
-        but.setCallbackData("test");
-
-        row.add(but);
-        rows.add(row);
-        inlineKeyboardMarkup.setKeyboard(rows);
-
-        editMessageText.setMessageId(messageId);
-        editMessageText.setChatId(String.valueOf(chatId));
-        editMessageText.setText(LocalDateTime.now().toString());
-        editMessageReplyMarkup.setReplyMarkup(inlineKeyboardMarkup);
-
-        try {
-            execute(editMessageText);
-            execute(editMessageReplyMarkup);
-        }
-        catch (TelegramApiException ignored)
-        {
-
-        }
     }
 
     private void deleteMessage(long chatId, Integer messageId)
